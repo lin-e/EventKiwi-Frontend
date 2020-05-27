beba5c8b0ac0e054fb767500febbb2f16a8f267e
Raihaan Rahman <raihaan2000@gmail.com>
2020-05-27 19:47:27 +0100

    Added a folder for constants

---
+++ src/constants/types.ts
@@ -0,0 +1,9 @@
+export type EventCardDetails = {
+   eventName: string, 
+   organiser: string, 
+   image: string, 
+   eventLocation: string, 
+   eventTime: string,
+   tags: string[],
+   id: string;
+}
