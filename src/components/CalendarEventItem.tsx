173c895f87075e0d2ad6d1e0d000822e228b91bb
Prabhjot Singh Grewal <psg918@ic.ac.uk>
2020-05-27 20:24:41 +0100

    Designed component for displaying basic information about an event within the Event page's calendar view

---
+++ src/components/CalendarEventItem.tsx
@@ -0,0 +1,45 @@
+import React, { MouseEvent } from 'react';
+import { EventDetails } from '../constants/types'
+import { IonItemSliding, IonGrid, IonCol, IonRow, IonItem, IonLabel, IonCardTitle, IonCardHeader, IonText, IonItemOptions } from '@ionic/react';
+import { getTime, getShortDate, sameDay } from '../utils/DateTimeTools'
+import './CalendarEventItem.css'
+
+interface CalendarEventItemProps {
+  event: EventDetails
+}
+
+const CalendarEventItem: React.FC<CalendarEventItemProps> = ({ event: event }) => {
+  const barCol = {"--socCol" : event.organiser.colour}
+
+  return (
+    <IonItemSliding>
+      <IonItem detail={true}>
+        <IonGrid className="eventItem" style={barCol}>
+          <IonRow className="eventName">
+            <div>
+              {event.name}
+            </div>
+          </IonRow>
+          <IonRow>
+            <IonCol size="6" className="detailCol">
+              <IonText color="medium">
+                <p className="eventDateTime">
+                  {getTime(event.datetimeStart)}
+                  &nbsp;&mdash;&nbsp;
+                  {(!sameDay(event.datetimeStart, event.datetimeEnd) ? getShortDate(event.datetimeEnd) + ", " : "") + getTime(event.datetimeEnd)}
+                </p>
+              </IonText>
+            </IonCol>
+            <IonCol size="6" className="detailCol">
+              <IonText color="medium">
+                <p className="eventLocation">{event.location}</p>
+              </IonText>
+            </IonCol>
+          </IonRow>
+        </IonGrid>
+      </IonItem>
+    </IonItemSliding>
+  )
+}
+
+export default CalendarEventItem
