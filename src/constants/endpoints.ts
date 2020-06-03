82719bd1b9625c61eb57b7ae0a40522bd1087914
Raihaan Rahman <raihaan2000@gmail.com>
2020-06-03 18:06:28 +0100

    Changed endpoint constants to take in parameters

--- src/constants/endpoints.ts
+++ src/constants/endpoints.ts
@@ -5,13 +5,17 @@ export const endpointURL = stagingURL;
 
 export const discoverEventCardURL = `${endpointURL}/events/cards/all`
 
-export const eventDetailsURL = `${endpointURL}/events/details/`
+export const eventDetailsURL = (id: string) => `${endpointURL}/events/details/${id}`
 export const eventResourcesURL = `${endpointURL}/events/resources/`
-export const resourceDownloadURL = `${productionURL}/file/get/`
+export const resourceDownloadURL = (id: string) => `${productionURL}/file/get/${id}`
 
 export const authEndpoint = `${endpointURL}/auth/new/`;
 export const deAuthEndpoint = `${endpointURL}/auth/end/`;
 
-export const goingToEventEndpoint = `${endpointURL}/events/going/`;
-export const interestedInEventEndpoint = `${endpointURL}/events/interested/`;
-export const notGoingToEventEndpoint = `${endpointURL}/events/none/`;
+export const goingToEventEndpoint = (id: string) =>  `${endpointURL}/events/going/${id}`;
+export const interestedInEventEndpoint = (id: string) =>  `${endpointURL}/events/interested/${id}`;
+export const notGoingToEventEndpoint = (id: string) =>  `${endpointURL}/events/none/${id}`;
+
+export const eventPostsEndpoint = (eventId: string, lastPostId: string) => {
+ return `${endpointURL}/${eventId}/${lastPostId}/`
+}
