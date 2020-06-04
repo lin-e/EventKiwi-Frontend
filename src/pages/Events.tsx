1a9694f98e14dc18903db72d61f7f9c089955474
Raihaan Rahman <raihaan2000@gmail.com>
2020-06-04 22:12:08 +0100

    Changed Events component to a functional component so that loading the events can be done when the user token loads in

--- src/pages/Events.tsx
+++ src/pages/Events.tsx
@@ -1,11 +1,9 @@
-import React, { Component, createRef } from 'react';
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, withIonLifeCycle, IonRefresher, IonRefresherContent } from '@ionic/react';
+import React, { useEffect } from 'react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonRefresher, IonRefresherContent, useIonViewDidEnter } from '@ionic/react';
 import './Events.css';
 import CalendarEventView from '../components/Calendar/CalendarEventView';
-import { ThunkDispatch } from 'redux-thunk';
 import { fetchCalEvents } from '../data/actions/actions'
 import { loadBlankEvent } from '../data/actions/viewEvent/viewEventActions';
-import { bindActionCreators } from 'redux';
 import { connect, ConnectedProps } from 'react-redux';
 import { RootState } from '../data/reducers';
 import { Redirect } from 'react-router';
@@ -20,51 +18,41 @@ const connector = connect(mapStateToProps, { fetchCalEvents, loadBlankEvent });
 
 type PropsFromRedux = ConnectedProps<typeof connector>;
 type EventsProps = PropsFromRedux
+const Events: React.FC<EventsProps> = (props) => {
 
-class Events extends Component<EventsProps> {
-  refresherRef: React.RefObject<HTMLIonRefresherElement>;
+  const refresherRef = React.useRef<HTMLIonRefresherElement>(null);
 
-  constructor(props: EventsProps) {
-    super(props);
-    this.refresherRef = createRef<HTMLIonRefresherElement>();
-    this.refresh = this.refresh.bind(this);
+  const refresh = () => {
+    props.fetchCalEvents(refresherRef.current!, props.userToken);
   }
 
-  componentDidUpdate() {
-    this.refresh()
+  useEffect(() => { refresh() }, [props.userToken])
+
+  useIonViewDidEnter(() => {
+    props.loadBlankEvent("events");
+  });
+
+
+  if (!props.isLoggedIn && !props.isLoading) {
+    return <Redirect to="/auth" />
   }
 
-  refresh() {
-    this.props.fetchCalEvents(this.refresherRef.current!, this.props.userToken);
-  }
-
-  ionViewWillEnter() {
-    this.props.loadBlankEvent("events");
-  }
-
-  render() {
-
-    if (!this.props.isLoggedIn && !this.props.isLoading) {
-      return <Redirect to="/auth" />
-    }
-
-    return (
-      <IonPage>
+  return (
+    <IonPage>
       <IonHeader>
         <IonToolbar>
           <IonTitle>Your Events</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent>
-        <IonRefresher ref={this.refresherRef} slot="fixed" onIonRefresh={this.refresh}>
+        <IonRefresher ref={refresherRef} slot="fixed" onIonRefresh={refresh}>
           <IonRefresherContent></IonRefresherContent>
         </IonRefresher>
 
-          <CalendarEventView />
+        <CalendarEventView />
       </IonContent>
     </IonPage>
-    );
-  }
+  );
 }
 
-export default connector(withIonLifeCycle(Events));
+export default connector(Events);
