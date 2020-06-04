6ca25195226515e7e216e8099e60e91dbed4c4b2
Raihaan Rahman <raihaan2000@gmail.com>
2020-06-04 14:55:30 +0100

    Removed going button from view event and moved it into event description component

--- src/components/ViewEventComponents/EventDescription.tsx
+++ src/components/ViewEventComponents/EventDescription.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { IonText, IonCard, IonCardSubtitle, IonCol, IonGrid, IonRow, IonButton, IonIcon } from '@ionic/react';
+import React, { useState } from 'react';
+import { IonText, IonCard, IonCardSubtitle, IonCol, IonGrid, IonRow, IonButton, IonIcon, IonToast } from '@ionic/react';
 import './EventDescription.css';
 import { Container, Row, Col } from 'react-grid-system';
 import ExpandTextView from '../ExpandTextView';
@@ -8,14 +8,19 @@ import { getDateRange } from '../../utils/DateTimeTools';
 import { RootState } from '../../data/reducers';
 import { ConnectedProps, connect } from 'react-redux';
 import { checkmarkCircleOutline, starOutline } from 'ionicons/icons';
+import { INTERESTED, GOING } from '../../constants/constants';
+import { goingToEvent, interestedInEvent, notGoingToEvent } from '../../data/actions/viewEventActions';
 
 const mapStateToProps = (state: RootState) => ({
    event: state.viewEvent.event,
    eventsEvent: state.viewEvent.eventsEvent,
-   discoverEvent: state.viewEvent.discoverEvent
+   discoverEvent: state.viewEvent.discoverEvent,
+   dGoingStatus: state.viewEvent.discoverEvent.goingStatus,
+   eGoingStatus: state.viewEvent.eventsEvent.goingStatus,
+   userToken: state.userDetails.userToken
 })
 
-const connector = connect(mapStateToProps)
+const connector = connect(mapStateToProps, { goingToEvent, interestedInEvent, notGoingToEvent })
 
 interface OwnProps {
    hide: boolean,
@@ -27,6 +32,32 @@ type EventDescriptionProps = PropsFromRedux & OwnProps;
 
 const EventDescription: React.FC<EventDescriptionProps> = (props) => {
    const eventDescription = props.tab === "events" ? props.eventsEvent : (props.tab === "discover" ? props.discoverEvent : props.event);
+   const goingStatus = props.tab === "events" ? props.dGoingStatus : (props.tab === "discover" ? props.dGoingStatus : props.goingToEvent);
+
+   const [goingToast, showGoingToast] = useState<boolean>(false);
+   const [notGoingToast, showNotGoingToast] = useState<boolean>(false);
+   const [interestedToast, showInterestedToast] = useState<boolean>(false);
+
+   const interestedClicked = () => {
+      if (eventDescription.goingStatus !== INTERESTED) {
+        props.interestedInEvent(eventDescription.id, props.tab, props.userToken);
+        showInterestedToast(true);
+      } else {
+        props.notGoingToEvent(eventDescription.id, props.tab, props.userToken);
+        showNotGoingToast(true);
+      }
+    }
+  
+    const goingClicked = () => {
+      if (eventDescription.goingStatus !== GOING) {
+        props.goingToEvent(eventDescription.id, props.tab, props.userToken);
+        showGoingToast(true)
+      } else {
+        props.notGoingToEvent(eventDescription.id, props.tab, props.userToken);
+        showNotGoingToast(true);
+      }
+    }
+
    return (
       <div style={props.hide ? { display: "none" } : {}}>
          <Container>
@@ -48,8 +79,12 @@ const EventDescription: React.FC<EventDescriptionProps> = (props) => {
                      </Col>
                      <Col lg={7}>
                         <br />
-                        <IonButton color="success">Going&nbsp; <IonIcon icon={checkmarkCircleOutline} /></IonButton>
-                        <IonButton color="medium">Interested&nbsp; <IonIcon icon={starOutline} /></IonButton>
+                        <IonButton onClick={goingClicked} color={goingStatus === GOING ? "success" : "medium"}>
+                           Going&nbsp; <IonIcon icon={checkmarkCircleOutline} />
+                        </IonButton>
+                        <IonButton onClick={interestedClicked} color={goingStatus === INTERESTED ? "warning" : "medium"}>
+                           Interested&nbsp; <IonIcon icon={starOutline} />
+                        </IonButton>
                      </Col>
                   </Row>
                   <Row>
@@ -63,14 +98,14 @@ const EventDescription: React.FC<EventDescriptionProps> = (props) => {
 
             {eventDescription.sameSocEvents.length > 0 &&
                <div>
-                  <IonText><h2>More from {eventDescription.organiser.name}</h2></IonText>
+                  <IonText><h2>More from {eventDescription.organiser.shortName}</h2></IonText>
                   <div className="suggestedEvents">
                      <IonGrid>
                         <IonRow>
                            {eventDescription.sameSocEvents.map(event => {
                               return <IonCol size="auto" key={`sameSocMiniEventCardCol--${event.id}`}>
 
-                                 <EventMiniCard 
+                                 <EventMiniCard
                                     key={`sameSocMiniEventCard--${event.id}`}
                                     tab={props.tab}
                                     eventId={event.id}
@@ -95,7 +130,7 @@ const EventDescription: React.FC<EventDescriptionProps> = (props) => {
                         <IonRow>
                            {eventDescription.similarEvents.map(event => {
                               return <IonCol size="auto" key={`similarEventMiniEventCardCol--${event.id}`}>
-                                 <EventMiniCard 
+                                 <EventMiniCard
                                     key={`similarEventMiniEventCard--${event.id}`}
                                     tab={props.tab}
                                     eventId={event.id}
@@ -111,6 +146,27 @@ const EventDescription: React.FC<EventDescriptionProps> = (props) => {
                   </div>
                </div>}
          </Container>
+
+         <IonToast
+            isOpen={goingToast}
+            onDidDismiss={() => { showGoingToast(false) }}
+            message="Added event to calendar."
+            duration={3000}
+         />
+
+         <IonToast
+            isOpen={goingToast || interestedToast}
+            onDidDismiss={() => { showInterestedToast(false) }}
+            message="Added event to calendar."
+            duration={3000}
+         />
+
+         <IonToast
+            isOpen={notGoingToast}
+            onDidDismiss={() => showNotGoingToast(false)}
+            message="Removed event from calendar."
+            duration={3000}
+         />
       </div>
    )
 }
--- src/pages/ViewEvent.tsx
+++ src/pages/ViewEvent.tsx
@@ -1,15 +1,14 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import { IonContent, IonPage, IonHeader, IonToolbar, IonButtons, IonBackButton, IonSegment, IonSegmentButton, IonLabel, IonIcon, IonFab, IonFabButton, IonFabList, IonToast } from '@ionic/react'
 import EventDescription from '../components/ViewEventComponents/EventDescription';
 import "./ViewEvent.css";
 import EventPostsList from '../components/ViewEventComponents/EventPostsList';
 import EventResourcesList from '../components/ViewEventComponents/EventResourcesList';
-import { checkmarkCircleOutline, caretUp, bulbOutline, shareOutline } from 'ionicons/icons';
+import { shareOutline } from 'ionicons/icons';
 import { connect, ConnectedProps } from 'react-redux';
 import { loadEventDetails, loadingEvent, loadBlankEvent, goingToEvent, interestedInEvent, notGoingToEvent } from '../data/actions/viewEventActions';
 import { loadEventPosts } from '../data/actions/eventPostsActions';
 import { RootState } from '../data/reducers';
-import { NOT_GOING, GOING, INTERESTED } from '../constants/constants';
 import { isPlatform } from '@ionic/react';
 import { Plugins } from '@capacitor/core';
 const { Share } = Plugins;
@@ -43,12 +42,6 @@ const ViewEvent: React.FC<ViewEventProps> = (props) => {
   const posts = segment === 'posts';
   const resources = segment === 'resources';
 
-  const fabColour = props.goingStatus > NOT_GOING ? (props.goingStatus !== GOING - 1 ? "success" : "warning") : "primary";
-  const fabIcon = props.goingStatus > NOT_GOING ? (props.goingStatus !== GOING - 1 ? checkmarkCircleOutline : bulbOutline) : caretUp;
-
-  const [goingToast, showGoingToast] = useState<boolean>(false);
-  const [notGoingToast, showNotGoingToast] = useState<boolean>(false);
-  const [interestedToast, showInterestedToast] = useState<boolean>(false);
   const [shareUrlToast, showShareUrlToast] = useState<boolean>(false);
 
   const shareUrlTextRef = useRef<HTMLTextAreaElement>(null);
@@ -92,26 +85,6 @@ const ViewEvent: React.FC<ViewEventProps> = (props) => {
       case 'resources':
         setResourcesY(y);
         break;
-    }
-  }
-
-  const interestedClicked = () => {
-    if (props.goingStatus !== INTERESTED) {
-      props.interestedInEvent(props.eventId, props.activeTab, props.userToken);
-      showInterestedToast(true);
-    } else {
-      props.notGoingToEvent(props.eventId, props.activeTab, props.userToken);
-      showNotGoingToast(true);
-    }
-  }
-
-  const goingClicked = () => {
-    if (props.goingStatus !== GOING) {
-      props.goingToEvent(props.eventId, props.activeTab, props.userToken);
-      showGoingToast(true)
-    } else {
-      props.notGoingToEvent(props.eventId, props.activeTab, props.userToken);
-      showNotGoingToast(true);
     }
   }
 
@@ -166,56 +139,19 @@ const ViewEvent: React.FC<ViewEventProps> = (props) => {
         <EventPostsList hide={!posts} />
 
         <EventResourcesList hide={!resources} />
-{/* 
-        {props.isLoggedIn &&
-          <IonFab vertical="bottom" horizontal="end" slot="fixed">
-            <IonFabButton color={fabColour}>
-              <IonIcon icon={fabIcon} />
-            </IonFabButton>
 
-            <IonFabList side="top">
-              <IonFabButton onClick={shareClicked} color="primary">
-                <IonIcon icon={shareOutline} />
-              </IonFabButton>
+        <IonFab vertical="bottom" horizontal="end" slot="fixed">
+          <IonFabButton onClick={shareClicked} color="primary">
+            <IonIcon icon={shareOutline} />
+          </IonFabButton>
+        </IonFab>
 
-              <IonFabButton onClick={interestedClicked} color={props.goingStatus === INTERESTED ? "warning" : ""}>
-                <IonIcon icon={bulbOutline} />
-              </IonFabButton>
-
-              <IonFabButton onClick={goingClicked} color={props.goingStatus === GOING ? "success" : ""}>
-                <IonIcon icon={checkmarkCircleOutline} />
-              </IonFabButton>
-            </IonFabList>
-          </IonFab>} */}
-
-          <IonFab vertical="bottom" horizontal="end" slot="fixed">
-            <IonFabButton onClick={shareClicked} color="primary">
-              <IonIcon icon={shareOutline} />
-            </IonFabButton>
-          </IonFab>
-
-
-
-        {/* <IonToast
-          isOpen={interestedToast || goingToast}
-          onDidDismiss={() => { showInterestedToast(false); showGoingToast(false) }}
-          message="Added event to calendar."
-          duration={3000}
-        />
-
-        <IonToast
-          isOpen={notGoingToast}
-          onDidDismiss={() => showNotGoingToast(false)}
-          message="Removed event from calendar."
-          duration={3000}
-        /> */}
 
         <IonToast
           isOpen={shareUrlToast}
           onDidDismiss={() => showShareUrlToast(false)}
           message="Event URL copied to clipboard."
-          duration={3000}
-        />
+          duration={3000} />
 
         {/* Text area used for copying share url to clipboard */}
         <textarea readOnly hidden={true} ref={shareUrlTextRef} id="shareUrl" value={shareUrl} />
