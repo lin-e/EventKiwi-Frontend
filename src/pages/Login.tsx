0c80a9e6797428da71d0318bbb66dfb89973126c
Raihaan Rahman <raihaan2000@gmail.com>
2020-06-05 12:57:40 +0100

    Centred the log in button on desktop safari, chrome and firefox

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -6,6 +6,7 @@ import { RootState } from '../data/reducers';
 import { Redirect, RouteComponentProps } from 'react-router';
 import MicrosoftLogin from "react-microsoft-login";
 import { logIn } from "../data/actions/userActions";
+import { Container } from 'react-grid-system';
 
 const mapStateToProps = (state: RootState) => ({
    loggedIn: state.userDetails.isLoggedIn
@@ -37,7 +38,7 @@ const Login: React.FC<LoginProps> = (props, state) => {
    return (
       <IonPage>
          <IonContent className="sk">
-            <IonGrid className="test">
+            <IonGrid className="centred">
                <IonRow>
                   <IonCol sizeLg="4" pushLg="4" sizeMd="8" pushMd="2">
                      <IonCard color="imperial">
@@ -49,10 +50,13 @@ const Login: React.FC<LoginProps> = (props, state) => {
                         </IonCardHeader>
                         <IonCardContent>
                            <IonRow>
-                              <MicrosoftLogin
-                                 className="horizontalCentre"
-                                 clientId="2e0eaa17-56a7-48a7-9a41-1757cc5e120e"
-                                 authCallback={authHandler} />
+                              <Container>
+                                 <MicrosoftLogin
+                                    className="horizontalCentre"
+                                    clientId="2e0eaa17-56a7-48a7-9a41-1757cc5e120e"
+                                    authCallback={authHandler} />
+
+                              </Container>
                            </IonRow>
                         </IonCardContent>
                         <br />
