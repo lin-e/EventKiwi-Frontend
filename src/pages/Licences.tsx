601c9ae4e4b4c8d8b7f00b47ceaa3db7e65bb98b
Raihaan Rahman <raihaan2000@gmail.com>
2020-06-08 20:16:33 +0100

    Added more licences

--- src/pages/Licences.tsx
+++ src/pages/Licences.tsx
@@ -138,6 +138,41 @@ const Licences: React.FC = () => {
                   <IonRow>
                      <IonCol>
                         <IonText>
+                           <h4>React scripts</h4>
+                        </IonText>
+
+                        <div>
+                           <p className="licenseInfo">
+                              MIT License<br /><br />
+
+                              Copyright (c) 2013-present, Facebook, Inc.<br /><br />
+
+                              Permission is hereby granted, free of charge, to any person obtaining a copy
+                              of this software and associated documentation files (the "Software"), to deal
+                              in the Software without restriction, including without limitation the rights
+                              to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+                              copies of the Software, and to permit persons to whom the Software is
+                              furnished to do so, subject to the following conditions:<br /><br />
+
+                              The above copyright notice and this permission notice shall be included in all
+                              copies or substantial portions of the Software.<br /><br />
+
+                              THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+                              IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+                              FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+                              AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+                              LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+                              OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+                              SOFTWARE.
+
+                           </p>
+                        </div>
+                     </IonCol>
+                  </IonRow>
+
+                  <IonRow>
+                     <IonCol>
+                        <IonText>
                            <h4>Redux</h4>
                         </IonText>
 
@@ -378,6 +413,106 @@ const Licences: React.FC = () => {
                   <IonRow>
                      <IonCol>
                         <IonText>
+                           <h4>Node.js</h4>
+                        </IonText>
+
+                        <div>
+                           <p className="licenseInfo">
+                              Copyright Node.js contributors. All rights reserved.<br /><br />
+
+                              Permission is hereby granted, free of charge, to any person obtaining a copy
+                              of this software and associated documentation files (the "Software"), to
+                              deal in the Software without restriction, including without limitation the
+                              rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+                              sell copies of the Software, and to permit persons to whom the Software is
+                              furnished to do so, subject to the following conditions:<br /><br />
+
+                              The above copyright notice and this permission notice shall be included in
+                              all copies or substantial portions of the Software.<br /><br />
+
+                              THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+                              IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+                              FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+                              AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+                              LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+                              FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+                              IN THE SOFTWARE.
+
+                           </p>
+                        </div>
+                     </IonCol>
+                  </IonRow>
+
+                  <IonRow>
+                     <IonCol>
+                        <IonText>
+                           <h4>Node-fetch</h4>
+                        </IonText>
+
+                        <div>
+                           <p className="licenseInfo">
+                              The MIT License (MIT)<br /><br />
+
+                              Copyright (c) 2016 - 2020 Node Fetch Team<br /><br />
+
+                              Permission is hereby granted, free of charge, to any person obtaining a copy of this
+                              software and associated documentation files (the "Software"), to deal in the Software
+                              without restriction, including without limitation the rights to use, copy, modify, merge,
+                              publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons
+                              to whom the Software is furnished to do so, subject to the following conditions:<br /><br />
+
+                              The above copyright notice and this permission notice shall be included in all copies or
+                              substantial portions of the Software.<br /><br />
+
+                              THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+                              BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+                              NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+                              DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+                              OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+                           </p>
+                        </div>
+                     </IonCol>
+                  </IonRow>
+
+                  <IonRow>
+                     <IonCol>
+                        <IonText>
+                           <h4>Reselect</h4>
+                        </IonText>
+
+                        <div>
+                           <p className="licenseInfo">
+                              The MIT License (MIT)<br /><br />
+
+                              Copyright (c) 2015-2018 Reselect Contributors<br /><br />
+
+                              Permission is hereby granted, free of charge, to any person obtaining a copy
+                              of this software and associated documentation files (the "Software"), to deal
+                              in the Software without restriction, including without limitation the rights
+                              to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+                              copies of the Software, and to permit persons to whom the Software is
+                              furnished to do so, subject to the following conditions:<br /><br />
+
+                              The above copyright notice and this permission notice shall be included in all
+                              copies or substantial portions of the Software.<br /><br />
+
+                              THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+                              IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+                              FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+                              AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+                              LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+                              OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+                              SOFTWARE.
+
+                           </p>
+                        </div>
+                     </IonCol>
+                  </IonRow>
+
+                  <IonRow>
+                     <IonCol>
+                        <IonText>
                            <h4>Font Awesome Free</h4>
                         </IonText>
 
@@ -417,6 +552,33 @@ const Licences: React.FC = () => {
                               trademarks does not indicate endorsement of the trademark holder by Font
                               Awesome, nor vice versa. **Please do not use brand logos for any purpose except
                               to represent the company, product, or service to which they refer.**<br /><br />
+
+                           </p>
+                        </div>
+                     </IonCol>
+                  </IonRow>
+
+                  <IonRow>
+                     <IonCol>
+                        <IonText>
+                           <h4>Typescript</h4>
+                        </IonText>
+
+                        <div>
+                           <p className="licenseInfo">
+                              Copyright 2014-present Microsoft
+
+                              Licensed under the Apache License, Version 2.0 (the "License");
+                              you may not use this file except in compliance with the License.
+                              You may obtain a copy of the License at
+
+                              http://www.apache.org/licenses/LICENSE-2.0
+
+                              Unless required by applicable law or agreed to in writing, software
+                              distributed under the License is distributed on an "AS IS" BASIS,
+                              WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+                              See the License for the specific language governing permissions and
+                              limitations under the License.
 
                            </p>
                         </div>
