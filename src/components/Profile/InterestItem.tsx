40e9351bc51830361d5405477bc79a886311c7e3
Prabhjot Singh Grewal <psg918@ic.ac.uk>
2020-06-11 23:44:46 +0100

    Added checks to ensure only valid interests can be added for searching interests on the profile page

--- src/components/Profile/InterestItem.tsx
+++ src/components/Profile/InterestItem.tsx
@@ -1,5 +1,5 @@
 import React, { MouseEvent, useState, useEffect } from 'react';
-import { IonGrid, IonCol, IonItem, IonRow, IonLabel, IonButton } from '@ionic/react';
+import { IonGrid, IonCol, IonItem, IonRow, IonLabel, IonButton, IonToast } from '@ionic/react';
 import { InterestDetails } from '../../constants/types';
 import { addProfileInterest, removeProfileInterest } from '../../data/actions/actions';
 import { ConnectedProps, connect } from 'react-redux';
@@ -24,7 +24,21 @@ type InterestItemProps = OwnProps & PropsFromRedux
 
 const InterestItem: React.FC<InterestItemProps> = ({ interest, interestList, userToken, addProfileInterest, removeProfileInterest }) => {
   const [interested, setInterested] = useState(interestList.includes(interest.name));
-  
+  const [tooLongToast, setTooLongToast] = useState(false);
+  const [invalidCharacterToast, setInvalidCharacterToast] = useState(false);
+
+  const validInterest = (intr: string) => {
+    const rejectRegex = /[`~!@#$%^&*()_|+\-=?;:'",.<>\{\}\[\]\\\/]/gi;
+    if (intr.length > 32) {
+      setTooLongToast(true);
+    } else if (rejectRegex.test(intr)) {
+      setInvalidCharacterToast(true);
+    } else {
+      return true;
+    }
+    return false;
+  }
+
   const toggleInterest = (e: MouseEvent) => {
     e.preventDefault();
 
@@ -32,8 +46,10 @@ const InterestItem: React.FC<InterestItemProps> = ({ interest, interestList, use
       removeProfileInterest(interest.name, userToken)
       setInterested(false);
     } else {
-      addProfileInterest(interest.name, userToken)
-      setInterested(true)
+      if (validInterest(interest.name)) {
+        addProfileInterest(interest.name, userToken)
+        setInterested(true)
+      }
     }
   }
 
@@ -42,24 +58,40 @@ const InterestItem: React.FC<InterestItemProps> = ({ interest, interestList, use
   }, [interest]);
   
   return (
-    <IonItem>
-      <IonGrid>
-        <IonRow>
-          <IonCol size="8">
-            <IonLabel>
-              <div className="interestName">{interest.name}</div>
-              <p>{`${interest.numInterested} ${(interest.numInterested == 1) ? "person" : "people"} interested`}</p>
-            </IonLabel>
-          </IonCol>
-          <IonCol size="4">
-            {interested ?
-              <IonButton color="danger" fill="outline" className="interestBtn" onClick={toggleInterest}>Remove</IonButton> :
-              <IonButton color="primary" fill="solid" className="interestBtn" onClick={toggleInterest}>Add</IonButton>
-            }
-          </IonCol>
-        </IonRow>
-      </IonGrid>
-    </IonItem>
+    <>
+      <IonItem>
+        <IonGrid>
+          <IonRow>
+            <IonCol size="8">
+              <IonLabel>
+                <div className="interestName">{interest.name}</div>
+                <p>{`${interest.numInterested} ${(interest.numInterested == 1) ? "person" : "people"} interested`}</p>
+              </IonLabel>
+            </IonCol>
+            <IonCol size="4">
+              {interested ?
+                <IonButton color="danger" fill="outline" className="interestBtn" onClick={toggleInterest}>Remove</IonButton> :
+                <IonButton color="primary" fill="solid" className="interestBtn" onClick={toggleInterest}>Add</IonButton>
+              }
+            </IonCol>
+          </IonRow>
+        </IonGrid>
+      </IonItem>
+      <IonToast
+        isOpen={tooLongToast}
+        onDidDismiss={() => setTooLongToast(false)}
+        message="Interests can not be longer that 32 characters."
+        position="bottom"
+        duration={2500}
+      />
+      <IonToast
+        isOpen={invalidCharacterToast}
+        onDidDismiss={() => setInvalidCharacterToast(false)}
+        message={`Interests can not contain the following characters: \`~!@#$%^&*()_|+-=?;:'",.<>{}[]\\/`}
+        position="bottom"
+        duration={2500}
+      />
+    </>
   );
 }
 
